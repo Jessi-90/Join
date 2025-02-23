cd4c6a71f2166f22c37de7e75e5d2a48edc99dd3
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-02-23 15:24:30 +0100

    added close function to close the overlay

--- scripts/addTaskOverlayBoard.js
+++ scripts/addTaskOverlayBoard.js
@@ -1,6 +1,18 @@
+/**
+ * Function to show the Add Task Overlay
+ */
 function showAddTaskOverlay() {
     let addTaskOverlayRef = document.getElementById('addTaskOverlay');
     addTaskOverlayRef.innerHTML = "";
     addTaskOverlayRef.innerHTML += showAddTaskOverlayHTMLTemplate();
     addTaskOverlayRef.classList.remove('d-none');
+}
+
+
+/**
+ * Function to close and Hide the Add Task Overlay
+ */
+function closeAddTaskOverlay() {
+    let addTaskOverlayRef = document.getElementById('addTaskOverlay');
+    addTaskOverlayRef.classList.add('d-none');
 }
