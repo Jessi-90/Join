c2cbf31816fa6a395fa40758dc40166d96279d52
Jessi-90 <jessicaschiminowski@gmail.com>
2025-02-26 11:04:24 +0100

    Edit-Contact-Overlay-Contacts

---
+++ scripts/templates/editContactOverlayTemplate.js
@@ -0,0 +1,45 @@
+function showEditContactOverlayHTMLTemplate() {
+return `
+ <div class="overlay" id="overlay-edit-contact">
+        <div class="modal">
+            <div class="sidebar">
+                <div class="logo-container">
+                    <img class="join-logo" src="../assets/img/Capa 2 (2).svg">
+                </div>
+                <h1>Edit contact</h1>
+                <div class="horizontal-line"></div>
+            </div>
+            <div class="form-container">
+                <button class="close-btn" onclick="closeOverlayEditContact()">✖</button>
+                <div class="profile-picture" id="profile-photo">
+                    <img src="../assets/img/circle_orange.svg" alt="Profilbild">
+                </div>
+                <form class="form">
+                    <div class="input-group">
+                        <input type="text" placeholder="Name" required>
+                        <span class="icon">
+                            <img src="../assets/icons/person_icon.svg" alt="User">
+                        </span>
+                    </div>
+                    <div class="input-group">
+                        <input type="email" placeholder="Email" required>
+                        <span class="icon">
+                            <img src="../assets/icons/mail_icon.svg" alt="Email">
+                        </span>
+                    </div>
+                    <div class="input-group">
+                        <input type="tel" placeholder="Phone">
+                        <span class="icon">
+                            <img src="../assets/icons/call.svg" alt="Phone">
+                        </span>
+                    </div>
+                    <div class="buttons">
+                        <button type="button" class="delete" onclick="closeOverlayEditContact()">Delete</button>
+                        <button type="submit" class="save">Save<img class="create-check"
+                                src="../assets/icons/check.svg"></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+   </div>
+   `}
