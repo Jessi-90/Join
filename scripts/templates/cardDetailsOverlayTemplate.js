980997c17c32004e8fe67330c58efa6efb2a7322
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-02-27 15:06:16 +0100

    added styling to card detail Overlay

--- scripts/templates/cardDetailsOverlayTemplate.js
+++ scripts/templates/cardDetailsOverlayTemplate.js
@@ -2,7 +2,7 @@ function cardDetailsOverlayHTMLTemplate(task) {
     return `
         <div class="board-card-detail-container">
             <div class="board-card-detail-header">
-                <span class="card-category user-story">${task.category}</span>
+                <span class="card-category ${task.category}">${task.category}</span>
                 <button class="close-btn"><img src="../assets/icons/close_btn.svg" alt="close" /></button>
             </div>
             <h4 class="card-detail-title">${task.title}</h4>
