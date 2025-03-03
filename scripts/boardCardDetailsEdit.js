4f280c2dd95eecf5c44e44c8f1383b48b7b3abac
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-03 20:14:52 +0100

    added scripts to show and close the edit overlay

---
+++ scripts/boardCardDetailsEdit.js
@@ -0,0 +1,37 @@
+/**
+ * Function to show the Edit Card Details Overlay and animate the appearance with a delay.
+ */
+function showBoardCardDetailsEdit(taskId) {
+    let boardCardOverlayRef = document.getElementById('boardCardDetails');
+    boardCardOverlayRef.innerHTML = "";
+    boardCardOverlayRef.innerHTML += cardDetailsEditOverlayHTMLTemplate(taskId);
+    boardCardOverlayRef.classList.remove('d-none');
+    setTimeout(() => {
+        let overlayContainerRef = document.querySelector('.board-card-edit-container');
+        overlayContainerRef.classList.add('show');
+    }, 10);
+}
+
+
+/**
+ * Closes the Edit Card Details Overlay, if clicked outside the container or the close button.
+ * Clicks inside the container prevent closing.
+ * The overlay will be hidden with a delay.
+ * 
+ * @param {Event} event - click-event.
+ */
+function closeBoardCardDetails(event) {
+    let overlay = document.getElementById('boardCardDetails');
+    let overlayContainer = document.querySelector('.board-card-edit-container');
+
+    if (event.target.closest('.board-card-edit-container') && !event.target.closest('.close-btn')) {
+        event.stopPropagation();
+        return;
+    }
+
+    overlayContainer.classList.remove('show');
+
+    setTimeout(() => {
+        overlay.classList.add('d-none');
+    }, 300);
+}
