a8d7c1b5e7addfd99d902e3d4c04585891b6d699
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-12 13:52:15 +0100

    Bug Fix: hide password on clicking the icon didn't work correct. Used mousedown instead of click event

--- scripts/signUpRevealPwd.js
+++ scripts/signUpRevealPwd.js
@@ -4,40 +4,41 @@
  * @param {string} iconId - The ID of the icon element.
  */
 function togglePasswordVisibility(inputId, iconId) {
-    let inputField = document.getElementById(inputId);
-    let inputIcon = document.getElementById(iconId);
-  
-    inputField.addEventListener("input", function () {
-      if (this.value.length > 0) {
-        this.classList.add("show-password");
-        inputIcon.src = "../assets/icons/visibility_off_icon.svg";
-      } else {
-        this.classList.remove("show-password", "password-visible");
-        inputIcon.src = "../assets/icons/lock_icon.svg";
-      }
-    });
-  
-    inputIcon.addEventListener("click", function () {
-      if (inputField.type === "password") {
-        inputField.type = "text";
-        inputIcon.src = "../assets/icons/visibility_icon.svg";
-      } else {
-        inputField.type = "password";
-        inputIcon.src = "../assets/icons/visibility_off_icon.svg";
-      }
-      inputField.focus();
-    });
-  
-    inputField.addEventListener("blur", function () {
-      this.type = "password";
-      if (this.value.length > 0) {
-        inputIcon.src = "../assets/icons/visibility_off_icon.svg";
-      } else {
-        inputIcon.src = "../assets/icons/lock_icon.svg";
-      }
-      this.classList.remove("password-visible");
-    });
-  }
-  
-  togglePasswordVisibility("signUpPassword", "signUpPasswordIcon");
-  togglePasswordVisibility("signUpConfirmPassword", "signUpConfirmPasswordIcon");
+	let inputField = document.getElementById(inputId);
+	let inputIcon = document.getElementById(iconId);
+
+	inputField.addEventListener("input", function () {
+		if (this.value.length > 0) {
+			this.classList.add("show-password");
+			inputIcon.src = "../assets/icons/visibility_off_icon.svg";
+		} else {
+			this.classList.remove("show-password", "password-visible");
+			inputIcon.src = "../assets/icons/lock_icon.svg";
+		}
+	});
+
+	inputIcon.addEventListener("mousedown", function (event) {
+		event.preventDefault();
+		if (inputField.type === "password") {
+			inputField.type = "text";
+			inputIcon.src = "../assets/icons/visibility_icon.svg";
+		} else {
+			inputField.type = "password";
+			inputIcon.src = "../assets/icons/visibility_off_icon.svg";
+		}
+		inputField.focus();
+	});
+
+	inputField.addEventListener("blur", function () {
+		this.type = "password";
+		if (this.value.length > 0) {
+			inputIcon.src = "../assets/icons/visibility_off_icon.svg";
+		} else {
+			inputIcon.src = "../assets/icons/lock_icon.svg";
+		}
+		this.classList.remove("password-visible");
+	});
+}
+
+togglePasswordVisibility("signUpPassword", "signUpPasswordIcon");
+togglePasswordVisibility("signUpConfirmPassword", "signUpConfirmPasswordIcon");
