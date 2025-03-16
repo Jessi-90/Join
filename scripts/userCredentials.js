a01e8d55b89910e63d8f17930b52f9c1b83b7e20
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-16 15:35:13 +0100

    added JSDoc

--- scripts/userCredentials.js
+++ scripts/userCredentials.js
@@ -1,17 +1,27 @@
+/**
+ * Retrieves the initials of the currently logged-in user from sessionStorage or localStorage.
+ * If no user is found, returns the default initials "G".
+ * 
+ * @returns {string} The initials of the currently logged-in user or "G" if no user is found.
+ */
 function getCurrentUserInitials() {
-let currentUser = sessionStorage.getItem("currentUser");
+    let currentUser = sessionStorage.getItem("currentUser");
 
-if (!currentUser) {
-    currentUser = localStorage.getItem("currentUser");
+    if (!currentUser) {
+        currentUser = localStorage.getItem("currentUser");
+    }
+
+    currentUser = currentUser ? JSON.parse(currentUser) : { initials: "G" };
+
+    let initials = currentUser.initials;
+
+    return initials;
 }
 
-currentUser = currentUser ? JSON.parse(currentUser) : { initials: "G"};
-
-let initials = currentUser.initials;
-
-return initials;
-}
-
+/**
+ * Renders the initials of the currently logged-in user on the user button.
+ * Retrieves the initials using the getCurrentUserInitials function and updates the user button's text and class.
+ */
 function renderCurrentUserInitials() {
     let initials = getCurrentUserInitials();
     let userButton = document.getElementById('userButton');
