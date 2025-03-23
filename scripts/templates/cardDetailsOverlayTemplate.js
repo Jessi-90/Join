f43fb59014e28f8e4c579fe7361919e647395ddd
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-23 12:10:00 +0100

    fixed checkbox status depending on completed value. Checkbox only checked if value === true

--- scripts/templates/cardDetailsOverlayTemplate.js
+++ scripts/templates/cardDetailsOverlayTemplate.js
@@ -81,7 +81,7 @@ function cardDetailAssigneeContentTemplate(task, assigneeId, taskId) {
 function cardDetailSubtasksContentTemplate(subtask) {
     return `
         <div class="card-deatil-subtasks-tasks">
-            <input type="checkbox" id="${subtask.id}" name="${subtask.id}" checked="${subtask.completed}">
+            <input type="checkbox" id="${subtask.id}" name="${subtask.id}" ${subtask.completed ? 'checked' : ''}>
             <label for="${subtask.id}">${subtask.title}</label>
         </div>
     `;
