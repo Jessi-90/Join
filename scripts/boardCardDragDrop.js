f5c3df94fa9d99a3dec94235837499b7735ee0e5
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-28 16:27:20 +0100

    added functions to show and remove the highlighting placeholder elemenet

--- scripts/boardCardDragDrop.js
+++ scripts/boardCardDragDrop.js
@@ -3,15 +3,41 @@ let currentDraggedCardId;
 
 function dragoverHandler(ev) {
     ev.preventDefault();
-  }
+    const columnCategory = ev.currentTarget.id;
+    const existingPlaceholder = document.getElementById('placeholder');
+    if (!existingPlaceholder || existingPlaceholder.parentElement.id !== columnCategory) {
+        highlightCardContainer(columnCategory);
+    }
+}
 
 function startCardDragging(taskId) {
     currentDraggedCardId = taskId;
-    console.log(currentTasksData);
-    console.log(currentDraggedCardId);
 }
 
-function moveCardTo(category) {
+function moveCardTo(category, columnCategory) {
     currentTasksData[currentDraggedCardId]['status'] = category;
+    removeHighlightCardContainer(columnCategory);
     renderTasks(currentTasksData);
+}
+
+function highlightCardContainer(columnCategory) {
+    const column = document.getElementById(columnCategory);
+    const existingPlaceholder = document.getElementById('placeholder');
+    if (!existingPlaceholder || existingPlaceholder.parentElement !== column) {
+        if (existingPlaceholder) {
+            existingPlaceholder.remove();
+        }
+        const placeholder = document.createElement('div');
+        placeholder.classList.add('highlight-card-container');
+        placeholder.id = 'placeholder';
+        column.appendChild(placeholder);
+    }
+}
+
+function removeHighlightCardContainer(columnCategory) {
+    const column = document.getElementById(columnCategory);
+    const placeholder = document.getElementById('placeholder');
+    if (placeholder && placeholder.parentElement === column) {
+        column.removeChild(placeholder);
+    }
 }
