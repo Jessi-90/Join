15e8964b60d96d3c90661faf77c2c392a8ac94f2
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-03-29 13:27:37 +0100

    general functions to get Contacts and Tasks from DB, outsourced

--- scripts/db.js
+++ scripts/db.js
@@ -3,3 +3,49 @@
  * @constant {string}
  */
 const BASE_URL = "https://da-join-project-default-rtdb.europe-west1.firebasedatabase.app/";
+
+
+/**
+ * Fetches tasks data from the server and updates the currentTasksData array.
+ * Uses the fetch API to get data from the specified endpoint.
+ * 
+ * @throws Will throw an error if the fetch operation fails or the response is not okay.
+ * @returns {Object} The updated currentTasksData object.
+ */
+async function fetchTasksData() {
+    try {
+        let databaseResponse = await fetch(BASE_URL + "tasks.json");
+        if (!databaseResponse.ok) {
+            throw new Error(`Status: ${databaseResponse.status}`);
+        }
+
+        currentTasksData = await databaseResponse.json();
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        currentTasksData = {};
+    }
+    return currentTasksData;
+}
+
+
+/**
+ * Fetches the current contacts from the database.
+ * @returns {Promise<Array>} an object with the contacts and the counter.
+ */
+async function getContacts(event) {
+    event.preventDefault();
+    let contacts;
+    try {
+        let response = await fetch(BASE_URL + "contacts.json");
+        if (!response.ok) {
+            throw new Error(`Status: ${response.status}`);
+        }
+        contacts = await response.json();
+        
+    } catch (error) {
+        console.error("Error while fetching conacts data", error);
+        contacts = {};
+    }
+    return contacts; 
+};
