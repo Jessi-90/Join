1380f8e9fdb4dbffa440a567e51c3988723b98f9
Jessi-90 <jessicaschiminowski@gmail.com>
2025-04-05 11:00:59 +0200

    subtasks

--- scripts/templates/cardDetailsEditOverlayTemplate.js
+++ scripts/templates/cardDetailsEditOverlayTemplate.js
@@ -39,25 +39,20 @@ function cardDetailsEditOverlayHTMLTemplate() {
                     <ul class="dropdown-options" id="dropdownOptions"></ul>
                 </div>
               <div id="selectedContactsContainer" class="selected-contacts-container"></div>
-              <label for="editCardSubtasks">Subtasks</label>
-              <div class="input-container">
-                  <input type="text" name="editCardSubtasks" id="subtasks" placeholder="Add new subtask">
-                  <button id="standard-subtask-btn" class="subtask-btn plus-btn" onclick="setFocusOnInput(event)">+</button>
-              </div>
-              <ul class="edit-card-subtasks-list" id="subtask-list">
-                  <li class="subtask-item">
-                      <span class="subtask-text">Example subtask</span>
-                      <div class="subtask-actions">
-                          <button class="subtask-edit-btn" onclick="editSubtask(this)">
-                              <img src="../assets/icons/subtask-edit.svg" alt="Edit">
-                          </button>
-                          <div class="subtask-separator"></div>
-                          <button class="subtask-delete-btn" onclick="deleteSubtask(this)">
-                          <img src="../assets/icons/subtask-delete.svg" alt="Delete">
-                          </button>
-                          </div>
-                      </li>
-                  </ul>              
+              <div class="form-group">
+                            <label for="subtasks">Subtasks</label>
+                            <div class="input-container">
+                                <input type="text" id="subtasks" class="input-field" placeholder="Add new subtask">
+                                <button id="standard-subtask-btn" class="subtask-btn plus-btn" onclick="setFocusOnInput(event)"></button>
+                                <div id="subtask-controls" class="subtask-nav d-none">
+                                    <button onclick="clearInputField(event)" id="clear-subtask-btn"
+                                        class="subtask-btn clear-subtask-btn"></button>
+                                    <div class="subtask-separator"></div>
+                                    <button id="add-subtask-btn" class="subtask-btn add-subtask-btn"></button>
+                                </div>
+                            </div>
+                            <ul id="subtask-list"></ul>
+                        </div>     
                  </div>
             </div>
             <div class="card-edit-footer">
