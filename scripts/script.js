8fead6508b8f6f86dede845facbc462c0d55db1e
Jessi-90 <jessicaschiminowski@gmail.com>
2025-04-06 12:00:20 +0200

    authGuard.js / path added

--- scripts/script.js
+++ scripts/script.js
@@ -258,20 +258,3 @@ document.addEventListener('DOMContentLoaded', function() {
       loginBtn.addEventListener('click', login);
   }
 });
-
-/**
- * Checks if a user or guest is logged in.
- * If not, redirects the user to the login page.
- */
-function checkIfUserIsLoggedIn() {
-  const userType = localStorage.getItem("userType");
-  const isOnLoginPage = window.location.pathname.includes("index.html") || window.location.pathname === "/" || window.location.pathname.endsWith("/");
-
-  if (userType !== "loggedIn" && userType !== "guest" && !isOnLoginPage) {
-    window.location.href = "./index.html";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  checkIfUserIsLoggedIn();
-});
