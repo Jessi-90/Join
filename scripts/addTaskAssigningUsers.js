e5b8f6756ed6e4156c6966a90f20bceb85ea6644
Jessi-90 <jessicaschiminowski@gmail.com>
2025-04-10 16:42:08 +0200

    Klammer

--- scripts/addTaskAssigningUsers.js
+++ scripts/addTaskAssigningUsers.js
@@ -236,6 +236,7 @@ function generateAssigneeHTML(task) {
     return Object.entries(assignedUsers).map(([id, name]) =>
         cardDetailAssigneeContentTemplate(task, id, task.id)
     ).join('');
+}
 
 /**
  * Moves the logged-in user to the top of the contacts list and adds "(You)" to the name.
@@ -258,5 +259,5 @@ function prioritizeLoggedInUser(contacts) {
 
     const filteredContacts = contacts.filter((_, i) => i !== index);
     return [loggedInContact, ...filteredContacts];
-  }
+ 
 }
