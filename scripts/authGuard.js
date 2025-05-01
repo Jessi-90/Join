462c0b95d869f7c5ed6e16358c26b309c0ac3eab
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-05-01 08:36:29 +0200

    adjusted check if user is logged in to reuse the functon

--- scripts/authGuard.js
+++ scripts/authGuard.js
@@ -3,14 +3,27 @@
  * If not, redirects the user to the login page.
  */
 function checkIfUserIsLoggedIn() {
-    const userType = localStorage.getItem("userType");
-    const isOnLoginPage = window.location.pathname === "/index.html" || window.location.pathname === "/";
-  
-    if (userType !== "loggedIn" && userType !== "guest" && !isOnLoginPage) {
-      window.location.href = "../index.html";
-    }
+  const userType = localStorage.getItem("userType");
+  const isOnLoginPage = window.location.pathname === "/index.html" || window.location.pathname === "/";
+  const isLoggedIn = userType === "loggedIn" || userType === "guest";
+
+  return {
+    isLoggedIn,
+    isNotLoggedIn: !isLoggedIn,
+    isNotOnLoginPage: !isOnLoginPage
+  };
+}
+
+
+function redirectToLoginPage() {
+  const loginCheck = checkIfUserIsLoggedIn();
+
+  if (loginCheck.isNotLoggedIn && loginCheck.isNotOnLoginPage ) {
+    window.location.href = "../index.html";
   }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    checkIfUserIsLoggedIn();
-  });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  redirectToLoginPage();
+});
