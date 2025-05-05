202ba31bde9692f72480fa5bc243de183dc4a294
Jessi-90 <jessicaschiminowski@gmail.com>
2025-05-05 14:50:58 +0200

    delete - renderTasks(currentTasksData); updateTasksInDatabase(currentTasksData);

--- scripts/boardCardDetails.js
+++ scripts/boardCardDetails.js
@@ -59,8 +59,6 @@ function closeBoardCardDetails(event) {
     overlayContainer.classList.remove("show");
     setTimeout(() => {
         overlay.classList.add('d-none');
-        updateTasksInDatabase(currentTasksData);
-        renderTasks(currentTasksData);
     }, 300);
 
     if (taskDetailsModified) {
